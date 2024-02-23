02f3db5636c290dda845e69f12becd3a8c837094
sparky9043 <sparky9043@gmail.com>
2024-02-23 18:35:01 +0900

    Add event listener to three buttons

--- 01-pomodoro/index.js
+++ 01-pomodoro/index.js
@@ -10,3 +10,13 @@ let time = 1500;
 let minute = Math.floor( time / 60 );
 let second = time % 60;
 
+// Button Event Listener
+startBtn.addEventListener("click", () => {
+  console.log("start");
+} );
+stopBtn.addEventListener("click", () => {
+  console.log("stop");
+} );
+resetBtn.addEventListener("click", () => {
+  console.log("reset");
+} );
