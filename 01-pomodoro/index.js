2557b5dd14ae3dc6d8b18def1deddb94a4d9f6e9
sparky9043 <sparky9043@gmail.com>
2024-02-23 18:55:24 +0900

    See previous git

--- 01-pomodoro/index.js
+++ 01-pomodoro/index.js
@@ -6,7 +6,7 @@ const resetBtn = document.querySelector("#reset-btn");
 
 // 25 min in seconds is 25*60 = 1500;
 let time = 1500;
-
+  
 // Button Event Listener
 startBtn.addEventListener("click", () => {
   setInterval( () => {
