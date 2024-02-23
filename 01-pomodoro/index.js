b82226004a53fab1531c7af8a371e4575386641e
sparky9043 <sparky9043@gmail.com>
2024-02-23 18:24:39 +0900

    Create access to time display and three buttons.

---
+++ 01-pomodoro/index.js
@@ -0,0 +1,6 @@
+// Create access to Time and Buttons
+const timeDisplay = document.querySelector(".time-display");
+const startBtn = document.querySelector("#start-btn");
+const stopBtn = document.querySelector("#stop-btn");
+const resetBtn = document.querySelector("#reset-btn");
+
