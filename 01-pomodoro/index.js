e9964ceabe9d67f38b3eabe389b493e949078d3e
sparky9043 <sparky9043@gmail.com>
2024-02-25 11:19:21 +0900

    Fix issue when pressing start twice

--- 01-pomodoro/index.js
+++ 01-pomodoro/index.js
@@ -6,23 +6,39 @@ const resetBtn = document.querySelector("#reset-btn");
 
 // 25 min in seconds is 25*60 = 1500;
 let interval;
-let time = 1445;
+let time = 10;
+
+// Track if the timer is already running
+let isRunning = false;
 
 // Button Event Listener
 startBtn.addEventListener("click", () => {
-  interval = setInterval( () => {
-    time--;
-    let minute = Math.floor( time / 60 );
-    let second = time % 60;
-    timeDisplay.textContent = `${minute.toString().padStart(2, "0")}:${second.toString().padStart(2, "0")}`;
-  }, 1000)
+  if (!isRunning) {
+    interval = setInterval( () => {
+      if ( time > 0 ) {
+        time--;
+        let minute = Math.floor( time / 60 );
+        let second = time % 60;
+        timeDisplay.textContent = `${minute.toString().padStart(2, "0")}:${second.toString().padStart(2, "0")}`;
+      } else {
+        alert("Time's Up!");
+        clearInterval(interval);
+        time = 1500;
+        timeDisplay.textContent = `25:00`;
+        isRunning = false;
+      }
+    }, 1000);
+    isRunning = true;
+  }
 } );
 
 stopBtn.addEventListener("click", () => {
   clearInterval(interval);
+  isRunning = false;
 } );
 
 resetBtn.addEventListener("click", () => {
   time = 1500;
   timeDisplay.textContent = `25:00`;
+  isRunning = false;
 } );
