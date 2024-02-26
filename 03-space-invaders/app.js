1f3dbc8ebe1c871adda57656990069b713aa2ef1
sparky9043 <sparky9043@gmail.com>
2024-02-26 22:16:52 +0900

    Create array from all divs within grid

--- 03-space-invaders/app.js
+++ 03-space-invaders/app.js
@@ -6,3 +6,9 @@ for (let i = 0; i < width * width; i++) {
   const square = document.createElement("div");
   grid.appendChild(square);
 }
+
+// Create an array from the squares so we can work with it in the JS file
+// This selects all "divs" that live inside the one with class grid
+const squares = Array.from(document.querySelectorAll(".grid div"));
+
+console.log(squares);
