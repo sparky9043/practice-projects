e0f6bf45a7c004769e137f143fd3ca60221cf91b
sparky9043 <sparky9043@gmail.com>
2024-04-19 15:22:10 +0900

    Create function to display result and update checkwinner

--- 08-rps/script.js
+++ 08-rps/script.js
@@ -1,6 +1,11 @@
 const buttons = document.querySelectorAll('div.container button');
+let win = 0;
+let lose = 0;
+let draw = 0;
 
-buttons.forEach((button) => button.addEventListener('click', startMatch));
+buttons.forEach((button) => {
+  if (button.id) button.addEventListener('click', startMatch);
+});
 
 function startMatch(e) {
   const rps = ['rock', 'paper', 'scissors'];
@@ -8,16 +13,36 @@ function startMatch(e) {
   const playerChoice = e.target.id;
   const computerChoice = rps[random];
   
-  checkWinner(playerChoice, computerChoice);
+  displayResults(playerChoice, computerChoice);
+}
+
+function displayResults(player, computer) {
+  const playerDisplay = document.querySelector('#player-display');
+  const computerDisplay = document.querySelector('#computer-display');
+  const resultDisplay = document.querySelector('#result-display');
+
+  playerDisplay.textContent = `Player chose ${player}`;
+  computerDisplay.textContent = `Computer chose ${computer}`;
+
+  const finalResult = checkWinner(player, computer);
+  resultDisplay.textContent = finalResult;
+
 }
 
 function checkWinner(player, computer) {
   // console.log(`Player chose ${player}. Computer chose ${computer}`);
-  if (player === computer) console.log('draw!');
-  else if (
+  if (player === computer) {
+    draw++;
+    return `It's a draw!`;
+  } else if (
     (player === 'rock' && computer === 'scissors') ||
     (player === 'paper' && computer === 'rock') ||
     (player === 'scissors' && computer === 'paper')
-  ) console.log('player wins!');
-  else console.log('computer wins!');
+  ) {
+    win++;
+    return `Player wins!`;
+  } else {
+    lose++;
+    return `Computer wins!`;
+  }
 }
