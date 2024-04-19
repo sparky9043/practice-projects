a8b3f89a992b4f67b747c3af2c1e326051b477cc
sparky9043 <sparky9043@gmail.com>
2024-04-19 15:06:00 +0900

    Create a functiont that correctly checks winner

--- 08-rps/script.js
+++ 08-rps/script.js
@@ -1,12 +1,23 @@
 const buttons = document.querySelectorAll('div.container button');
-const rps = ['rock', 'paper', 'scissors'];
 
 buttons.forEach((button) => button.addEventListener('click', startMatch));
 
 function startMatch(e) {
+  const rps = ['rock', 'paper', 'scissors'];
   const random = Math.floor( Math.random()*3 );
-  const playerChoice = this.id;
+  const playerChoice = e.target.id;
   const computerChoice = rps[random];
   
-  console.log(playerChoice, computerChoice);
+  checkWinner(playerChoice, computerChoice);
+}
+
+function checkWinner(player, computer) {
+  // console.log(`Player chose ${player}. Computer chose ${computer}`);
+  if (player === computer) console.log('draw!');
+  else if (
+    (player === 'rock' && computer === 'scissors') ||
+    (player === 'paper' && computer === 'rock') ||
+    (player === 'scissors' && computer === 'paper')
+  ) console.log('player wins!');
+  else console.log('computer wins!');
 }
